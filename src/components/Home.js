f54b000e08ca9f245717e09f549b4e250ddde29c
leecjaster <35693724+leecjaster@users.noreply.github.com>
2021-04-06 11:59:15 -0500

    add projects

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ import image from "../img/fiddle-fig.jpg";
 export default function Home() {
   return (
     <main>
-      <img src={image} alt="fiddle-fig" className="absolute object-cover w-full h-full"/>
-      <section>
-        <h1>Howdy, I'm Lee.</h1>
+      <img src={image} alt="Fiddle Fig" className="absolute object-cover w-full h-full"/>
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Howdy, I'm Lee.</h1>
       </section>
     </main>
   )
